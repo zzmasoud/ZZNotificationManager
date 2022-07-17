5255cb08a5328759bb484db0ddf337cb601aa945
Masoud Sh <zzmasoud@gmail.com>
2022-07-17 11:24:55 +0200

    check authorization delivers false on not authorized status

--- ZZNotificationManagerTests/ZZNotificationManagerTests.swift
+++ ZZNotificationManagerTests/ZZNotificationManagerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import UserNotifications
 
 protocol NotificationManager {
     
@@ -14,12 +15,57 @@ protocol NotificationManager {
 final class ZZNotificationManagerTests: XCTestCase {
     
     func test_init_doesNotAuthorize() {
-        let sut = SpyNM()
+        let (sut, _) = makeSUT()
         
         XCTAssertEqual(sut.authorizationCallCounts, 0)
     }
     
+    func test_checkAuthorization_deliversFalseOnNotAuthorized() {
+        let (sut, stub) = makeSUT()
+        stub.rejectAuthorization()
+        
+        let exp = expectation(description: "waiting for completion...")
+        sut.checkAuthorization { authorized in
+            XCTAssertFalse(authorized)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: SpyNM, notificationCeter: NotificationCenterStub) {
+        let stub = NotificationCenterStub()
+        let sut = SpyNM(notificationCenter: stub)
+        
+        return (sut, stub)
+    }
+    
     private class SpyNM: NotificationManager {
+        
+        let notificationCenter: NotificationCenterStub
         private(set) var authorizationCallCounts = 0
+        
+        init(notificationCenter: NotificationCenterStub) {
+            self.notificationCenter = notificationCenter
+        }
+        
+        func checkAuthorization(completion: (Bool) -> Void) {
+            let authorized = notificationCenter.authorize()
+            completion(authorized)
+        }
+    }
+    
+    private class NotificationCenterStub {
+        private var authorizationResponse = false
+        
+        func rejectAuthorization() {
+            authorizationResponse = false
+        }
+        
+        func authorize() -> Bool {
+            return authorizationResponse
+        }
     }
 }
