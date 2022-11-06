37d3fe72f30db1c706ec0ca7018354b3c6214e7a
Masoud Sh <zzmasoud@gmail.com>
2022-11-06 09:55:52 +0100

    `ZZNotificationContent` maps all parameters to proper `UNNotificationContent`

--- ZZNotificationManagerTests/ZZNotificationContentTests.swift
+++ ZZNotificationManagerTests/ZZNotificationContentTests.swift
@@ -6,13 +6,25 @@ import XCTest
 import UserNotifications
 
 public final class ZZNotificationContent {
-    private init() { }
+    private init() {}
     
     public static func map(title: String, categoryId: String, body: String? = nil, subtitle: String? = nil, badge: Int? = nil, soundName: String? = nil) -> UNNotificationContent {
         let content = UNMutableNotificationContent()
         content.title = title
         content.categoryIdentifier = categoryId
-
+        if let body = body {
+            content.body = body
+        }
+        if let subtitle = subtitle {
+            content.subtitle = subtitle
+        }
+        if let badge = badge {
+            content.badge = badge as NSNumber
+        }
+        if let soundName = soundName {
+            content.sound = UNNotificationSound(named: UNNotificationSoundName.init(soundName))
+        }
+        
         return content
     }
 }
@@ -27,5 +39,23 @@ class ZZNotificationContentTest: XCTestCase {
         
         XCTAssertEqual(sut.title, title)
         XCTAssertEqual(sut.categoryIdentifier, categoryId)
+    }
+    
+    func test_mapWithOptionalParameters_deliversSameValuesOnUNNotificationContent() {
+        let title = "Title"
+        let categoryId = UUID().uuidString
+        let body = "Body"
+        let subtitle = "Subtitle"
+        let badge = 22
+        let soundName = "123.mp3"
+        
+        let sut = ZZNotificationContent.map(title: title, categoryId: categoryId, body: body, subtitle: subtitle, badge: 22, soundName: soundName)
+        
+        XCTAssertEqual(sut.title, title)
+        XCTAssertEqual(sut.categoryIdentifier, categoryId)
+        XCTAssertEqual(sut.body, body)
+        XCTAssertEqual(sut.subtitle, subtitle)
+        XCTAssertEqual(sut.badge?.intValue, badge)
+        XCTAssertEqual(sut.sound, UNNotificationSound(named: .init(soundName)))
     }
 }
