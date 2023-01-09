89c7c3f9b82d1d6c77adc8937e5c027bb46e9478
Masoud Sh <zzmasoud@gmail.com>
2023-01-09 12:30:17 +0100

    Add proper error message on assertions

--- CLOCNotificationManagerTests/Shared/XCTestCase+Assertions.swift
+++ CLOCNotificationManagerTests/Shared/XCTestCase+Assertions.swift
@@ -8,9 +8,10 @@ import UserNotifications
 extension XCTestCase {
     // XCTAssert like this becuase comparing two `Array`s may fail because of orders and keeping orders is also important so I couldn't use `Set`
     func assertThat(_ notificationCenter: MockNotificationCenter, deletedNotificationRequestsWithIds ids: [String], file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(notificationCenter.deletedNotificationRequests.count, ids.count, file: file, line: line)
+        let actualRequests = notificationCenter.deletedNotificationRequests
+        XCTAssertEqual(actualRequests.count, ids.count, "expected to get \(ids.count) deleted requests but got (\(actualRequests.count) deleted requests.", file: file, line: line)
         ids.forEach { id in
-            XCTAssertTrue(notificationCenter.deletedNotificationRequests.contains(id), file: file, line: line)
+            XCTAssertTrue(actualRequests.contains(id), "expected to have deleted request with id \(id) but it isn't exist.", file: file, line: line)
         }
     }
     
@@ -27,21 +28,21 @@ extension XCTestCase {
     }
     
     func assertThat(_ notificationRequest: UNNotificationRequest, hasSameId id: String, andCategoryId categoryId: String, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(notificationRequest.identifier, id, file: file, line: line)
-        XCTAssertEqual(notificationRequest.content.categoryIdentifier, categoryId, file: file, line: line)
+        XCTAssertEqual(notificationRequest.identifier, id, "identifiers are not same.", file: file, line: line)
+        XCTAssertEqual(notificationRequest.content.categoryIdentifier, categoryId, "category identifiers are not same.", file: file, line: line)
     }
     
     func assertThat(_ notificationRequest: UNNotificationRequest, hasSameTitle title: String, andBody body: String?, at index: Int = 0, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(notificationRequest.content.title, title, file: file, line: line)
-        XCTAssertEqual(notificationRequest.content.body, body, file: file, line: line)
+        XCTAssertEqual(notificationRequest.content.title, title, "titles are not same.", file: file, line: line)
+        XCTAssertEqual(notificationRequest.content.body, body, "bodies are not same.", file: file, line: line)
     }
     
-    func assertThat(_ notificationRequest: UNNotificationRequest, hasSameFireDate fireDate: Date, file: StaticString = #file, line: UInt = #line) {
+    func assertThat(_ notificationRequest: UNNotificationRequest, hasSameFireDate expectedFireDate: Date, file: StaticString = #file, line: UInt = #line) {
         let trigger = notificationRequest.trigger
         guard let calendarTrigger = trigger as? UNCalendarNotificationTrigger else {
             return XCTFail("expected to get \(UNCalendarNotificationTrigger.self).", file: file, line: line)
         }
-        XCTAssertTrue(calendarTrigger.isEqual(toDate: fireDate, calendar: self.calendar), file: file, line: line)
+        XCTAssertTrue(calendarTrigger.isEqual(toDate: expectedFireDate, calendar: self.calendar), "expected to have fire date at \(expectedFireDate) but got \(calendarTrigger.dateComponents)", file: file, line: line)
     }
     
     private func getNotificationRequest(_ notificationCenter: MockNotificationCenter, at index: Int = 0) -> UNNotificationRequest {
